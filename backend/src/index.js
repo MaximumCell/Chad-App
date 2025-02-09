b110476f406e5931b97623b33d7d01e4124444a9
MaximumCell <alihassanwinner@gmail.com>
2025-02-09 19:47:44 +0500

    done with settign profile pic isssue remain

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import {connectDB} from "./lib/db.js"
-import cookieParser from "cookie-parser"
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 dotenv.config();
@@ -14,17 +14,17 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin: "http://localhost:5174",
-    credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server is running on port PORT "+ PORT);
-    connectDB();
+  console.log("Server is running on port PORT " + PORT);
+  connectDB();
 });
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,29 +1,20 @@
-import {
-  Eye,
-  EyeOff,
-  Loader2,
-  Lock,
-  Mail,
-  MessageSquare,
-  
-} from "lucide-react";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore.js";
+import AuthImagePattern from "../components/AuthImagePattern .jsx";
 
 const LoginPage = () => {
-  
-    const [showPassword, setShowPassword] = useState(false);
-    const [formData, setFormData]
-    = useState({
-    email:"",
-    password:"",
-    });
-    const { login, isLoggingIn } = useAuthStore();
-    const handleSubmit = async (e) => {
-    e.preventDefault() ;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { login, isLoggingIn } = useAuthStore();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     login(formData);
-    };
+  };
   return (
     <div className="h-screen grid lg:grid-cols-2">
       {/* left side form */}
@@ -33,37 +24,36 @@ const LoginPage = () => {
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary transition-colors">
-              <MessageSquare className='size-6 text-primary' />
-                </div>
-                <h1 className='text-2xl font-bold mt-2'>Welcome Back</h1>
-                <p className='text-base-content/60'>Sign into Your Account</p>
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
+              <p className="text-base-content/60">Sign into Your Account</p>
             </div>
           </div>
 
           {/* // Form */}
           <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="form-control">
-          <label className="label">
-            <span className="label-text font-medium">Email</span>
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <Mail className="size-5 text-base-content/40" />
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
             </div>
-            <input
-              type="email"
-              className={`input input-bordered w-full pl-10`}
-              placeholder="you@example.com"
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-            />
-          </div>
-        </div>
 
-
-        <div className="form-control">
+            <div className="form-control">
               <label className="label">
                 <span className="label-text font-medium">Password</span>
               </label>
@@ -95,7 +85,6 @@ const LoginPage = () => {
               </div>
             </div>
 
-
             <button
               type="submit"
               className="btn btn-primary w-full"
@@ -110,9 +99,8 @@ const LoginPage = () => {
                 "Sign in"
               )}
             </button>
-
-        </form>
-        <div className="text-center">
+          </form>
+          <div className="text-center">
             <p className="text-base-content/60">
               Create account?{" "}
               <Link to="/signup" className="link link-primary">
@@ -120,12 +108,18 @@ const LoginPage = () => {
               </Link>
             </p>
           </div>
+        </div>
+      </div>
 
-              </div>
-            </div>
-          </div>
-    
+      {/* Right Side - Image/Pattern */}
+      <AuthImagePattern
+        title={"Welcome back!"}
+        subtitle={
+          "Sign in to continue your conversations and catch up with your messages."
+        }
+      />
+    </div>
   );
-}
+};
 
-export default LoginPage
+export default LoginPage;
