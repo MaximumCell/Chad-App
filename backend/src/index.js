519a632780c5bd2801717442fcb3132cedf88f6b
MaximumCell <alihassanwinner@gmail.com>
2025-02-10 23:41:10 +0500

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,13 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
-
+import path, { join } from "path";
 dotenv.config();
 
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +25,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port PORT " + PORT);
